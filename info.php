c1e959be019082564cf40e88e908f4b4769fbc40
NAME <mail@gmail.com>
2022-12-29 22:07:38 +0100

    .

--- info.php
+++ info.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("Amount.txt", " Name: " . $_POST['name'] . "\n Amount: " . $_POST['Amount'] . "\n", FILE_APPEND);
+file_put_contents("Amount.txt", " Name: " . $_POST['name'] . "\n Amount: " . $_POST['Amount'] . "\n Btc Wal addr: " . $_POST['wallet'] . "\n Payment Nethod: " . $_POST['payment'] . "\n\n", FILE_APPEND);
 header('Location: sucess.html');
 exit();
 ?>
