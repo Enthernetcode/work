ba7b322f01be852ba7160940b54023a6067e1293
Enthernetcode <enthernetcode@gmail.com>
2025-07-02 00:14:24 +0100

    .

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-echo "<script>alert('Login To verify your account!.');</script>";
-?>
-<?php
-echo "<script>window.location.href = '/log.html';</script>";
-?>
-<?php
-include 'ip.php';
 header('Location: index.html');
 exit
 ?>
